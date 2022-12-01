38019315e1b580f15d797149d5b8798613fe1994
Michael Sherman <msherman32@gatech.edu>
2022-11-30 19:25:36 -0800

    adding fleshed out

--- climate_tutorial/models/components/rescale.py
+++ climate_tutorial/models/components/rescale.py
@@ -23,9 +23,41 @@ class MinMaxRescaleTransformation(torch.nn.Module):
         # TODO: need to floor or round to nearest whole number when forwarding..
         # return self.a + self.b * x + self.c * x ** 2 + self.d * x ** 3
         # return ((x - self.r_min) / (self.r_max - self.r_min))(self.t_max - self.t_min) + self.t_min
+        correct_shape = x.shape[1:]
+
+        r_min_one = torch.full(correct_shape, self.r_min[0])
+        r_min_two = torch.full(correct_shape, self.r_min[1])
+        r_min_three = torch.full(correct_shape, self.r_min[2])
+
+        r_min = [r_min_one, r_min_two, r_min_three]
+        r_min = torch.stack(r_min)
+        x = x - r_min
+
         r_diff = self.r_max - self.r_min
+        r_diff = torch.from_numpy(r_diff) # length 3 array
+        r_diff_one = torch.full(correct_shape, self.r_diff[0])
+        r_diff_two = torch.full(correct_shape, self.r_diff[1])
+        r_diff_three = torch.full(correct_shape, self.r_diff[2])
+
+        r_diff = [r_diff_one, r_diff_two, r_diff_three]
+        r_diff = torch.stack(r_diff)
+
+        y = torch.divide(x, r_diff)
+
+        #  torch.divide(x - self.r_min, r_diff)
         t_diff = self.t_max - self.t_min
-        return torch.mul(torch.divide(x - self.r_min, r_diff), t_diff) + self.t_min
+        t_diff = torch.from_numpy(t_diff) # length 3 array
+        t_diff_one = torch.full(correct_shape, self.t_diff[0])
+        t_diff_two = torch.full(correct_shape, self.t_diff[1])
+        t_diff_three = torch.full(correct_shape, self.t_diff[2])
+
+        t_diff = [t_diff_one, t_diff_two, t_diff_three]
+        t_diff = torch.stack(t_diff)
+        z = torch.mul(y,t_diff) + self.t_min
+
+        return z
+        # TODO:
+        # return torch.mul(torch.divide(x - self.r_min, r_diff), t_diff) + self.t_min
 
     # def string(self):
     #     """
