ad2d7a22fd726357e825b083a3e49532ba43f1c2
Michael Sherman <msherman32@gatech.edu>
2022-11-30 21:20:11 -0800

    adding more fixed

--- climate_tutorial/models/components/rescale.py
+++ climate_tutorial/models/components/rescale.py
@@ -19,44 +19,54 @@ class MinMaxRescaleTransformation(torch.nn.Module):
         In the forward function we accept a Tensor of input data and we must return
         a Tensor of output data. We can use Modules defined in the constructor as
         well as arbitrary operators on Tensors.
+
         """
+        # Essentially returns: return torch.mul(torch.divide(x - self.r_min, r_diff), t_diff) + self.t_min
+
         # TODO: need to floor or round to nearest whole number when forwarding..
         # return self.a + self.b * x + self.c * x ** 2 + self.d * x ** 3
         # return ((x - self.r_min) / (self.r_max - self.r_min))(self.t_max - self.t_min) + self.t_min
-        correct_shape = x.shape[1:]
+        # correct_shape = x.shape[1:]
 
-        r_min_one = torch.full(correct_shape, self.r_min[0])
-        r_min_two = torch.full(correct_shape, self.r_min[1])
-        r_min_three = torch.full(correct_shape, self.r_min[2])
+        # r_min_one = torch.full(correct_shape, self.r_min[0])
+        # r_min_two = torch.full(correct_shape, self.r_min[1])
+        # r_min_three = torch.full(correct_shape, self.r_min[2])
 
-        r_min = [r_min_one, r_min_two, r_min_three]
-        r_min = torch.stack(r_min)
+        # r_min = [r_min_one, r_min_two, r_min_three]
+        # r_min = torch.stack(r_min)
+        r_min_shape = len(self.r_min)
+        r_min = torch.from_numpy(self.r_min)
+        r_min = torch.reshape(r_min, (r_min_shape,1,1)) # all climate variable will come in as (C,x,y)
         x = x - r_min
 
         r_diff = self.r_max - self.r_min
-        r_diff = torch.from_numpy(r_diff) # length 3 array
-        r_diff_one = torch.full(correct_shape, r_diff[0])
-        r_diff_two = torch.full(correct_shape, r_diff[1])
-        r_diff_three = torch.full(correct_shape, r_diff[2])
+        r_diff_shape = len(r_diff) # length 3 array
+        r_diff = torch.from_numpy(r_diff) # length 3 tensor
+        # r_diff_one = torch.full(correct_shape, r_diff[0])
+        # r_diff_two = torch.full(correct_shape, r_diff[1])
+        # r_diff_three = torch.full(correct_shape, r_diff[2])
+        r_diff = torch.reshape(r_diff, (r_diff_shape,1,1)) 
+        # r_diff_one = torch.full(correct_shape, r_diff[0])
+        # r_diff_two = torch.full(correct_shape, r_diff[1])
+        # r_diff_three = torch.full(correct_shape, r_diff[2])
+        # r_diff = [r_diff_one, r_diff_two, r_diff_three]
+        # r_diff = torch.stack(r_diff)
 
-        r_diff = [r_diff_one, r_diff_two, r_diff_three]
-        r_diff = torch.stack(r_diff)
 
         y = torch.divide(x, r_diff)
 
-        #  torch.divide(x - self.r_min, r_diff)
-        t_diff = self.t_max - self.t_min
-        t_diff_one = torch.full(correct_shape, t_diff)
-        t_diff_two = torch.full(correct_shape, t_diff)
-        t_diff_three = torch.full(correct_shape, t_diff)
+        t_diff = self.t_max - self.t_min # scalar 
+        t_diff = torch.full(x.shape, t_diff)
+        # t_diff_one = torch.full(correct_shape, t_diff)
+        # t_diff_two = torch.full(correct_shape, t_diff)
+        # t_diff_three = torch.full(correct_shape, t_diff)
 
-        t_diff = [t_diff_one, t_diff_two, t_diff_three]
-        t_diff = torch.stack(t_diff)
+        # t_diff = [t_diff_one, t_diff_two, t_diff_three]
+        # t_diff = [t_diff_one, t_diff_one, t_diff_one]
+        # t_diff = torch.stack(t_diff)
         z = torch.mul(y, t_diff) + self.t_min
 
         return z
-        # TODO:
-        # return torch.mul(torch.divide(x - self.r_min, r_diff), t_diff) + self.t_min
 
     # def string(self):
     #     """
